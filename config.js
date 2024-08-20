d77c6a20d63f2664e0a9d2242c355d7828f197c5
Mohammad Qandeel <mohammad.qandeel@outlook.com>
2024-08-20 11:43:51 +0400

    Fix config file

--- config.js
+++ config.js
@@ -8,10 +8,10 @@ module.exports = {
   databaseSettings: {
     path: process.env.DB_PATH,
     fileName: process.env.DB_FILE_NAME,
-    poolMax: Number(process.env.DB_POOL_MAX) ?? 5,
-    poolMin: Number(process.env.DB_POOL_MIN) ?? 0,
-    poolAcquire: Number(process.env.DB_POOL_ACQUIRE) ?? 60000,
-    poolIdle: Number(process.env.DB_POOL_IDLE) ?? 10000,
+    poolMax: Number(process.env.DB_POOL_MAX ?? 5),
+    poolMin: Number(process.env.DB_POOL_MIN ?? 0),
+    poolAcquire: Number(process.env.DB_POOL_ACQUIRE ?? 60000),
+    poolIdle: Number(process.env.DB_POOL_IDLE ?? 10000),
     logging:
       process.env.DB_LOGGING && process.env.DB_LOGGING === "false"
         ? false
